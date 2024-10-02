af52ec9682c3f46f84f39b96cb44d27d02a685df
Dinis Soveral Botelho Rosa <dinisrosa22@gmail.com>
2024-10-02 22:18:07 +0100

    TPC3 update

--- TPC3/21fosforos.py
+++ TPC3/21fosforos.py
@@ -1,7 +1,9 @@
+import random
+
 #Create Menu:
 
 def menu():
-    print('''zdsdfsdfsd
+    print('''
 Bem vindo ao jogo dos 21 Fósforos!
 Neste jogo, tu e o computador, á vez, podem tirar 1, 2, 3 ou 4 fósforos.
 Se ficares com o último fósforo perdes!
@@ -25,14 +27,13 @@ def PlayerStart():
     while total_fosforos > 1:
         player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: '))
         while player_turn < 1 or player_turn > min(4,total_fosforos):
-            print(f'\033[1;31mNão é possível tirar esse número de fósforos! Por favor tire de 1 a {min(4,total_fosforos)} fósforos')
+            print(f'\033[1;31mNão é possível tirar esse número de fósforos! Por favor tire de 1 a {min(4,total_fosforos)} fósforos.')
             player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: '))
         total_fosforos -= player_turn
-        print(f'\033[1;30mFicaram/Ficou {total_fosforos} fósforos')
+        print(f'\033[1;30mFicaram/Ficou {total_fosforos} fósforos.')
         total_fosforos -= (5 - player_turn) 
-        print(f'\033[0;35mO pc tirou {5 - player_turn} fósforos!\n\033[1;30mFicaram/Ficou {total_fosforos} fósforos')
-    print('\033[1;33mO pc ganhou! Boa sorte para a próxima!\033[0;30m')
-
+        print(f'\033[0;35mO pc tirou {5 - player_turn} fósforos!\n\033[1;30mFicaram/Ficou {total_fosforos} fósforos.]')
+    print('\033[1;33mO pc ganhou! Boa sorte para a próxima!]\033[0;30m]')
 
 
 # O pc começa:
@@ -49,10 +50,10 @@ def PcStart():
     gameOver = False
 
     while total_fosforos > 1 and not gameOver:
-        #plogica para o player
+        #logica para o player
         player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: 1, 2, 3 ou 4: '))
         while player_turn < 1 or player_turn > min(4,total_fosforos):
-            print(f'\033[1;31mNão é possível tirar esse número de fósforos! Por favor tire de 1 a {min(4,total_fosforos)} fósforos')
+            print(f'\033[1;31mNão é possível tirar esse número de fósforos! Por favor tire de 1 a {min(4,total_fosforos)} fósforos.')
             player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: '))
 
         total_fosforos -= player_turn
@@ -70,7 +71,7 @@ def PcStart():
                 if total_fosforos % 5 == 1:
                     pc_turn = rd.randint(1, 4)
                 else:
-                    pc_turn = abs(3 - (total_fosforos % 5))
+                    pc_turn = (total_fosforos % 5) - (player_turn)
             
             total_fosforos -= pc_turn
             print(f'\033[0;35m{pc_turn}\n\033[1;30mT = {total_fosforos}')
@@ -84,35 +85,39 @@ def PcStart():
     if playerWin:
         print('\033[1;33mGanhaste! PARABENS!!!!')
     else:
-        print('\033[1;33mO pc ganhou! Boa sorte para a próxima!\033[0;30m')        
+        print('\033[1;33mO pc ganhou! Boa sorte para a próxima!\033[0;30m]')        
 
 
 
 
 
 def PcStart2():
-    gameOver = False
 
-    total_fosforos = 21
-    pc_turn = rd.randint(1, 4)
-    total_fosforos -= pc_turn
-    print(f'\033[0;35m{pc_turn}\n\033[1;30mT = {total_fosforos}')
+    fosforos = 21
 
-    player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: 1, 2, 3 ou 4: '))
-    while player_turn < 1 or player_turn > min(4, total_fosforos):
-        print(f'\033[1;31mNão é possível tirar esse número de fósforos! Por favor tire de 1 a {min(4, total_fosforos)} fósforos')
-        player_turn = int(input('\033[0;34mO teu turno, quantos fósforos queres tirar: '))
+    while fosforos > 0:
+        # Turno do computador
+        computador = (fosforos - 1) % 5
+        if computador == 0 or computador > fosforos:
+            computador = random.randint(1, min(4, fosforos))
+
+        fosforos -= computador
+        print(f"O computador retirou {computador} fósforos.")
+
+      
+        if fosforos == 1:
+            print("Sobrou apenas 1 fósforo. O computador ganhou!")
+        else:
+            # Turno do jogador
+            jogador = int(input(f"Há {fosforos} fósforos. Quantos deseja retirar (1-4)? "))
+            while jogador < 1 or jogador > 4 or jogador > fosforos:
+                jogador = int(input("Entrada inválida. Escolha um valor entre 1 e 4 que não exceda o número de fósforos restantes: "))
     
-    total_fosforos -= player_turn
-    print(f'\033[1;30mT = {total_fosforos}')
+            fosforos -= jogador
+            if fosforos == 1:
+                print("Sobrou 1 fósforo. Ganhaste!")
 
-    while total_fosforos > 1 and not gameOver:
-        if pc_turn + player_turn != 5:
-            pc_turn = 5 - (total_fosforos % 5)
-        
 
-        total_fosforos -= pc_turn
-        print(f'\033[0;35m{pc_turn}\n\033[1;30mT = {total_fosforos}')
 
 #O JOGO:
 
@@ -122,13 +127,13 @@ while modo_de_jogo != 'c':
     if modo_de_jogo == 'a':
         PlayerStart()
         menu()
-        modo_de_jogo = input('Que é que deseja fazer?')
+        modo_de_jogo = input('Que é que deseja fazer? ')
     elif modo_de_jogo == 'b':
         print('In development')
-        PcStart()
+        PcStart2()
         menu()
-        modo_de_jogo = input('Que é que deseja fazer?')
+        modo_de_jogo = input('Que é que deseja fazer? ')
     else:
         print('Opcão inválida Escolha uma opção válida, por favor!')
-        modo_de_jogo = input('Que é que deseja fazer?')
+        modo_de_jogo = input('Que é que deseja fazer? ')
 print('Obrigado! Volte sempre!')
--- TPC3/README.md
+++ TPC3/README.md
@@ -7,7 +7,4 @@ O TPC3 consistiu em emplementar o jogo dos 21 fósforos em python:
 * O jogador começa -> PlayerStart(): O PC irá ganhas sempre
 * O pc começa -> PcStart(): O PC perde se o jogador não fizer erros de calculo; se o mesmo fizer, o PC passa para a frente e ganha.
 
-Erro Atual: em PcStart()
-* quando o jogador faz um erro de calculo, o PC não consegue manter a liderança e acaba por quase perder, isto é: jogador joga e sobra 1 mas o pc vai jogar e fazer os fosforos ficarem negativos e dizer que ganha.
-
 
